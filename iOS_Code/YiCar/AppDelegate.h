59102a77e95eaf9fae4914d41c35181c26d454cb
Benjamin Young <byoungm@gmail.com>
2015-01-03 11:17:32 -0800

    Added Skelton Xcode Project

---
+++ iOS_Code/YiCar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YiCar
+//
+//  Created by Benjamin Young on 1/3/15.
+//  Copyright (c) 2015 Benjamin Young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS_Code/YiCar/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  YiCar
+//
+//  Created by Benjamin Young on 1/3/15.
+//  Copyright (c) 2015 Benjamin Young. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
