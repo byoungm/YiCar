0e9b1dcbebe9d46a10c4dcc841592841423e1722
Benjamin Young <byoungm@gmail.com>
2015-01-06 23:39:13 -0800

    Added working serial port to bluetooth code

--- MCU_Code/main.c
+++ MCU_Code/main.c
@@ -20,6 +20,7 @@ char usartString[20];
 
 
 void main() {
+    UINT8 rx[3];
 
     mcu_init();
     lcd_initDisplay();
@@ -28,8 +29,22 @@ void main() {
     sprintf(usartString,"Number:%d",5);
     putsUSART(usartString); //Delay10KTCYx(7) ???
 
-    while(1);
-    
+    while(1)
+    {
+        usartString[0] = 0x0B;
+        usartString[1] = 0x0;
+        usartString[2] = 0x08;
+        usartString[3] = '\0';
+
+        putsUSART(usartString);
+
+        while(!DataRdyUSART());// Wait for data to be received
+        getsUSART(rx,3);
+        sprintf(usartString,"%02x,%02x,%02x",rx[0],rx[1],rx[2]);
+        lcd_writeLine(usartString, 1);
+    }
+
+    CloseUSART();
 }
 
 
@@ -38,7 +53,8 @@ void mcu_init()
     // Internal osc. 8 MHz
     OSCCON = 0b01110000;
 
-    // USART INIT
+    // USART INIT, BAUD = 57600 (8MHz clock)
     OpenUSART(USART_TX_INT_OFF & USART_RX_INT_OFF & USART_ASYNCH_MODE &
-               USART_EIGHT_BIT & USART_CONT_RX & USART_BRGH_HIGH, 25);
+               USART_EIGHT_BIT & USART_CONT_RX & USART_BRGH_HIGH, 8);
+
 }
